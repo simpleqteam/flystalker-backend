be0ca7c1a0f5eb0af5309428641543f501b374e1
Mihail Gorbunov <mgorbunov@okko.tv>
2020-03-22 11:24:49 +0300

    FLSTKBACK-1 add initial FlyStalkerHttpClient interface and implementation

---
+++ src/main/kotlin/xyz/simpleq/flystalker/service/FlyStalkerHttpClient.kt
@@ -0,0 +1,5 @@
+package xyz.simpleq.flystalker.service
+
+interface FlyStalkerHttpClient {
+    fun sendHttpRequest()
+}
---
+++ src/main/kotlin/xyz/simpleq/flystalker/service/impl/FlyStalkerHttpClientImpl.kt
@@ -0,0 +1,12 @@
+package xyz.simpleq.flystalker.service.impl
+
+import org.springframework.stereotype.Service
+import xyz.simpleq.flystalker.service.FlyStalkerHttpClient
+
+@Service
+class FlyStalkerHttpClientImpl : FlyStalkerHttpClient {
+    override fun sendHttpRequest() {
+        TODO("Not yet implemented")
+    }
+
+}
