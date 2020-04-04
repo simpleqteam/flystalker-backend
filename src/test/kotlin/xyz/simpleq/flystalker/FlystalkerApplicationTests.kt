b122427f8f258dcad52713b5cab07e368b7d3e2a
Mikhail Gorbunov <mihail.grb@gmail.com>
2020-04-04 23:39:12 +0300

    FLSTKBACK-1 remove obsolete

--- src/test/kotlin/xyz/simpleq/flystalker/FlystalkerApplicationTests.kt
+++ src/test/kotlin/xyz/simpleq/flystalker/FlystalkerApplicationTests.kt
@@ -2,9 +2,6 @@ package xyz.simpleq.flystalker
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
-import reactor.core.publisher.Mono
-//TODO(не импортируется, нада чтобы импортировалось)
-//import xyz.simpleq.flystalker.service
 
 @SpringBootTest
 class FlystalkerApplicationTests {
@@ -12,19 +9,4 @@ class FlystalkerApplicationTests {
     @Test
     fun contextLoads() {
     }
-
-    @Test
-    fun sendRequest(){
-//
-//        val httpRequest = HttpRequest()
-//        httpRequest.url ="https://www.google.com/"
-//        httpRequest.method = HttpMethod.GET
-//
-//        val response: Mono<Response> = FlyStalkerHttpClientImpl().sendHttpRequest(httpRequest)
-//        response
-//                .map(Response::statusCode)
-//                .subscribe(System.out::println)
-
-    }
-
 }
