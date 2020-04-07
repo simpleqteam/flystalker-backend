aaa69a43e6323fb80a7ac6e5be318aa929cc1aae
Mikhail Gorbunov <mihail.grb@gmail.com>
2020-04-07 22:38:48 +0300

    FLSTKBACK-4 exchange entity and repository

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,6 +31,7 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+    implementation("com.vladmihalcea:hibernate-types-4:2.9.7")
 
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
 
---
+++ src/main/kotlin/xyz/simpleq/flystalker/persistance/entities/FSExchangeEntity.kt
@@ -0,0 +1,45 @@
+package xyz.simpleq.flystalker.persistance.entities
+
+import com.fasterxml.jackson.databind.JsonNode
+import com.vladmihalcea.hibernate.type.json.JsonNodeBinaryType
+import org.hibernate.annotations.Type
+import org.hibernate.annotations.TypeDef
+import java.time.Duration
+import java.time.OffsetDateTime
+import java.time.ZoneOffset
+import java.util.*
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.Id
+
+
+@Entity
+@TypeDef(name = "jsonb", typeClass = JsonNodeBinaryType::class)
+data class FSExchangeEntity(
+    @Id
+    val id: UUID = UUID.randomUUID(),
+
+    val creationDateTime: OffsetDateTime = OffsetDateTime.now(ZoneOffset.UTC),
+
+    val method: HttpMethod,
+
+    val hostAndPath: String,
+
+    @Type(type = "jsonb")
+    @Column(columnDefinition = "jsonb")
+    val headers: JsonNode,
+
+    @Type(type = "jsonb")
+    @Column(columnDefinition = "jsonb")
+    val queryParams: JsonNode,
+
+    val sendAfterDateTime: OffsetDateTime
+) {
+    enum class HttpMethod {
+        GET, POST, PUT, DELETE, HEAD, OPTIONS;
+
+        companion object
+    }
+
+    companion object
+}
